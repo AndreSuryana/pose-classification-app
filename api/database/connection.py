bb9f027326195503028a8667210c826a6ca0043c
AndreSuryana <Andresuryana17@gmail.com>
2024-07-30 16:36:10 +0700

    Refactor Database connection as singleton object

--- api/database/connection.py
+++ api/database/connection.py
@@ -4,14 +4,28 @@ import os
 import logger as log
 
 class Database:
+
+    _instance = None
+
+    def __new__(cls, *args, **kwargs):
+        if cls._instance is None:
+            cls._instance = super(Database, cls).__new__(cls, *args, **kwargs)
+        return cls._instance
+
     def __init__(self):
-        base_dir = os.path.dirname(os.path.abspath(__file__))
-        db_name = os.getenv('API_DB', 'database.db')
-        self.db_path = os.path.join(base_dir, db_name)
-        self.migrations_dir = os.path.join(base_dir, 'migrations')
+        if not hasattr(self, 'initialized'): # To ensure initialization happens only once
+            base_dir = os.path.dirname(os.path.abspath(__file__))
+            db_name = os.getenv('API_DB', 'database.db')
+            self.db_path = os.path.join(base_dir, db_name)
+            self.migrations_dir = os.path.join(base_dir, 'migrations')
+            self._migrations_applied = False
+            self.initialized = True  # Mark the instance as initialized
 
     def apply_migrations(self):
         """Apply database migrations."""
+        if self._migrations_applied:
+            return # Avoid re-applying migrations
+
         log.i('Starting database migration process.')
 
         conn = sqlite3.connect(self.db_path)
@@ -42,18 +56,15 @@ class Database:
                         log.i(f'Migration {filename} applied successfully.')
 
         conn.close()
+        self._migrations_applied = True
         log.i('Database migration process completed.')
 
     def get_connection(self):
         """Get a connection to the SQLite database."""
-        if not os.path.exists(self.db_path):
-            log.i('Database not found. Applying migrations.')
-        else:
-            log.i('Database found. Checking for required migrations.')
-
-        self.apply_migrations()
-
         try:
+            # Apply migrations when needed
+            self.apply_migrations()
+
             conn = sqlite3.connect(self.db_path)
             conn.row_factory = sqlite3.Row
             log.i('Database connection established.')
